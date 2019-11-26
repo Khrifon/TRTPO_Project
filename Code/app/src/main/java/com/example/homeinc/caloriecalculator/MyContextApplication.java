d371947c11447263391c1e52f711eb94118613bd
Khrifon <khristoforova.asya@gmail.com>
2019-11-26 14:34:54 +0300

    Restore code

---
+++ Code/app/src/main/java/com/example/homeinc/caloriecalculator/MyContextApplication.java
@@ -0,0 +1,15 @@
+package java.com.example.homeinc.caloriecalculator;
+
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+
+import java.com.example.homeinc.caloriecalculator.tabLayout.fragments.CalculatorFragment;
+
+public class MyContextApplication {
+
+    public static ViewPager viewPager;
+    public static Context context;
+    public static CalculatorFragment calculatorFragment;
+
+}
